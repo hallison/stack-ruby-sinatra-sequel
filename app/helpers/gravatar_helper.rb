5c7048e6856c8a114d93defa1e8cb2f9a56cb897
Hallison Batista <hallisonbatista@gmail.com>
2017-02-05 17:59:21 -0400

    Gravatar helper

---
+++ app/helpers/gravatar_helper.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+module Boilerplate
+
+module GravatarHelper
+  def usuario_email
+    session[:user] && session[:user][:email]
+  end
+
+  def gravatar(email, options = {})
+    hash = Digest::MD5.hexdigest(email || 'noemail@gravatar.com')
+    params = options.map{|k,v| "#{k}=#{v}"}.join('&')
+    "http://www.gravatar.com/avatar/#{hash}?#{params}"
+  end
+end
+
+end # Boilerplate
