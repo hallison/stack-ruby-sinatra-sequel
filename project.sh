6a2ff172a1911c2975b78c2c9d427532bff8997c
Hallison Batista <hallisonbatista@gmail.com>
2018-08-01 10:45:43 -0400

    Small improvements

--- project.sh
+++ project.sh
@@ -4,10 +4,10 @@ source_module="Boilerplate"
 target_name=${1:?project dirname}
 target_module=${2:?project module name}
 
-mv lib/boilerplate lib/${target_name}
-mv lib/boilerplate.rb lib/${target_name}.rb
+mv lib/${source_name} lib/${target_name}
+mv lib/${source_name}.rb lib/${target_name}.rb
 
-grep -lir boilerplate . | while read filename; do
+grep -lir ${source_name} . | while read filename; do
   sed -i.bkp -r "s/${source_name}/${target_name}/" "${filename}"
   sed -i.bkp -r "s/${source_module}/${target_module}/" "${filename}"
 done
